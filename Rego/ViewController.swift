4f6c8719d354fa62d90b9abdeba1305330eb3fd6
Rohit Saini <sketchlearn7@gmail.com>
2020-06-15 23:05:23 +0530

    Initial Commit

---
+++ Rego/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rego
+//
+//  Created by Rohit Saini on 15/06/20.
+//  Copyright © 2020 Rohit Saini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
